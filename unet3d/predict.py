6ccef7f23a2a0669e056e2358b7b185a9e9df285
eanemo <eaguilera@nemotec.org>
2021-09-21 09:49:15 +0200

    Debug

--- unet3d/predict.py
+++ unet3d/predict.py
@@ -461,7 +461,7 @@ def pytorch_volumetric_predictions(model_filename, model_name, n_features, filen
             x_image, ref_image = load_images_from_dataset(dataset, idx, resample_predictions)
 
             batch.append(get_nibabel_data(x_image))
-            print("filenames", dataset.epoch_filenames)
+            print("filenames", dataset.epoch_filenames[idx])
             batch_references.append((x_image, ref_image))
             batch_subjects.append(subject_id)
             batch_filenames.append(x_filename)
