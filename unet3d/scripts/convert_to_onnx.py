409e66a04b2fdf833dc5b7f43217ab440491f85d
eanemo <eaguilera@nemotec.org>
2021-11-24 08:56:10 +0100

    Formateo del código

--- unet3d/scripts/convert_to_onnx.py
+++ unet3d/scripts/convert_to_onnx.py
@@ -4,8 +4,10 @@ import json
 import numpy as np
 from unet3d.models.pytorch.build import build_or_load_model
 
+
 def convert(args):
-    dummy_input = torch.tensor(torch.randn(2, 1, 96, 96, 96, device='cuda')).float()
+    dummy_input = torch.tensor(torch.randn(
+        2, 1, 96, 96, 96, device='cuda')).float()
     fp_json = open(args.config)
     config = json.load(fp=fp_json)      # Devuelve un diccionario
     model_name = config['model_name']
@@ -22,14 +24,16 @@ def convert(args):
     model = build_or_load_model(model_name=model_name, model_filename=args.model_filename, n_outputs=num_outputs,
                                 n_features=n_features, n_gpus=1, strict=True, **model_kwargs)
 
-    torch.onnx.export(model, dummy_input, "model.onnx", verbose=True, input_names=["encoder.layers.0.blocks.0.conv1.norm1"], output_names=["final_convolution"])
-
+    torch.onnx.export(model, dummy_input, "model.onnx", verbose=True, input_names=[
+                      "encoder.layers.0.blocks.0.conv1.norm1"], output_names=["final_convolution"])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, help="Fichero de configuración usado durante el entrenamiento", required=True)
-    parser.add_argument("--model_filename", type=str, help="Fichero que contiene el modelo guardado que queremos convertir", required=True)
+    parser.add_argument(
+        "--config", type=str, help="Fichero de configuración usado durante el entrenamiento", required=True)
+    parser.add_argument("--model_filename", type=str,
+                        help="Fichero que contiene el modelo guardado que queremos convertir", required=True)
     parser.add_argument("--converted_filename", type=str,
                         help="Fichero que contiene el modelo convertido a formato ONNX", required=True)
 
