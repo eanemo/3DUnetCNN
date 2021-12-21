d815f11eaae0489ad77dbc1dcf6cc82304da6ed0
eanemo <eaguilera@nemotec.org>
2021-12-21 12:20:33 +0100

    Se activa el entorno

--- train.sh
+++ train.sh
@@ -3,5 +3,8 @@
 # Se exporta el PythonPath
 export PYTHONPATH=${PWD}:${PYTHONPATH}
 
+# Se activa el entorno
+source env/bin/activate
+
 # Ejecutamos
 python unet3d/scripts/train.py --config_filename path_to_results/config.json --model_filename path_to_results/model.h5 --training_log_filename path_to_results/output_log.csv
